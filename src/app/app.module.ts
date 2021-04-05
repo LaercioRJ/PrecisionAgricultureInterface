6af4040f6b18698f1dff02bb04db18877b99c0f5
unknown <laerciorjunior@gmail.com>
2021-04-05 20:38:52 -0300

    rectification 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { MaterialModule } from './material-module';
+import { NgModule } from '@angular/core';
 
 import { CardsDisplayComponent } from './main-menu/cards-display/cards-display.component';
-import { LayerCardComponent } from './main-menu/layer-card/layer-card.component';
-import { TopBarComponent } from './main-menu/top-bar/top-bar.component';
 import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
-import { TablePageComponent } from './layer-visualization/table/table-page/table-page.component';
-import { ZmLayerInfoComponent } from './layer-visualization/table/layer-info/zm-layer-info/zm-layer-info.component';
-import { SpLayerInfoComponent } from './layer-visualization/table/layer-info/sp-layer-info/sp-layer-info.component';
-import { SpTableComponent } from './layer-visualization/table/data-tables/sp-table/sp-table.component';
+import { LayerCardComponent } from './main-menu/layer-card/layer-card.component';
 import { MzTableComponent } from './layer-visualization/table/data-tables/mz-table/mz-table.component';
 import { RectificationComponent } from './precision-agriculture/rectification/rectification.component';
+import { SpLayerInfoComponent } from './layer-visualization/table/layer-info/sp-layer-info/sp-layer-info.component';
+import { SpTableComponent } from './layer-visualization/table/data-tables/sp-table/sp-table.component';
+import { TablePageComponent } from './layer-visualization/table/table-page/table-page.component';
+import { TopBarComponent } from './main-menu/top-bar/top-bar.component';
+import { ZmLayerInfoComponent } from './layer-visualization/table/layer-info/zm-layer-info/zm-layer-info.component';
+
+
 
 @NgModule({
   declarations: [
-    TopBarComponent,
     CardsDisplayComponent,
-    LayerCardComponent,
     ConfirmationDialogComponent,
-    TablePageComponent,
-    ZmLayerInfoComponent,
-    SpLayerInfoComponent,
-    SpTableComponent,
+    LayerCardComponent,
     MzTableComponent,
     RectificationComponent,
+    SpLayerInfoComponent,
+    SpTableComponent,
+    TablePageComponent,
+    TopBarComponent,
+    ZmLayerInfoComponent,
   ],
   imports: [
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
+    FormsModule,
     MaterialModule
   ],
   providers: [],
--- src/app/precision-agriculture/rectification/rectification.component.ts
+++ src/app/precision-agriculture/rectification/rectification.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Layer } from '../../classes/layer';
+
+import { LayerStorageService } from '../../services/layer-storage.service';
 
 @Component({
   selector: 'app-rectification',
@@ -7,9 +12,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RectificationComponent implements OnInit {
 
-  constructor() { }
+  customIteration = 7;
+  customIterationPreviousValue = 7;
+  iterations: number[] = [1, 2, 3, 4, 5, 6];
+  kernelFormats: string[] = ['Retangular', 'Elipse', 'Cruz'];
+  kernelSizes: string[] = ['3X3', '5X5', '7X7', '9X9', '11X11'];
+  rectificationMethods: string[] = ['Mediana', 'Aberto', 'Fechado', 'Aberto e Fechado'];
+  selectedKernelFormat = 'Retangular';
+  selectedKernelSize = '3X3';
+  selectedIteration = 1;
+  selectedRectificationMethod = 'Mediana';
+
+  layer!: Layer;
+
+  loadBarState = 'none';
+
+  constructor(private layerStorage: LayerStorageService,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // tslint:disable-next-line: deprecation
+    this.activatedRoute.paramMap.subscribe(parameters => {
+      this.layer = this.layerStorage.getLayer(Number(parameters.get('layerIndex')));
+    });
+  }
+
+  executeRectification(): void {
+    let kFormat: string;
+    let kSize: number;
+    let rMethod: string;
+    let iteration: number;
+    this.loadBarState = 'block';
+
+    switch (this.selectedKernelFormat) {
+      case 'Retangular':
+        kFormat = 'rect';
+        break;
+      case 'Elipse':
+        kFormat = 'ellipse';
+        break;
+      case 'Cruz':
+        kFormat = 'cross';
+        break;
+    }
+
+    switch (this.selectedKernelSize) {
+      case '3X3':
+        kSize = 3;
+        break;
+      case '5X5':
+        kSize = 5;
+        break;
+      case '7X7':
+        kSize = 7;
+        break;
+      case '9X9':
+        kSize = 9;
+        break;
+      case '11X11':
+        kSize = 11;
+        break;
+    }
+
+    switch (this.selectedRectificationMethod) {
+      case 'Mediana':
+        rMethod = 'median';
+        break;
+      case 'Aberto':
+        rMethod = 'open';
+        break;
+      case 'Fechado':
+        rMethod = 'close';
+        break;
+      case 'Aberto e Fechado':
+        rMethod = 'openandclose';
+        break;
+    }
+
+    if (Number(this.selectedIteration) === 7) {
+      iteration = this.customIteration;
+    } else {
+      iteration = Number(this.selectedIteration);
+    }
   }
 
 }
