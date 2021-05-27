f2044fc093a0496b9c0ebe9a5c2534651266ee56
unknown <laerciorjunior@gmail.com>
2021-05-26 22:04:41 -0300

    table-paginator 3

--- src/app/layer-visualization/table/data-tables/sp-table/sp-table.component.ts
+++ src/app/layer-visualization/table/data-tables/sp-table/sp-table.component.ts
@@ -18,19 +18,21 @@ export class SpTableComponent implements OnInit {
 
   actualContournTablePage = 1;
   contournDisplayedColumns!: string[];
+  contournPointsPerPage = 20;
   contournLatitudeHeader!: string;
   contournLongitudeHeader!: string;
   contournTableDataSource = new MatTableDataSource<number[]>();
-  contournTableLowerIndex = 0;
-  contournTableUpperIndex = 20;
+  contournTableLowerIndex = (this.contournPointsPerPage * -1);
+  contournTableUpperIndex = 0;
   hasContourn = false;
   totalContournTablePages = 0;
 
   actualDatasetTablePage = 1;
   datasetDisplayedColumns!: string[];
+  datasetPointsPerPage = 20;
   datasetTableDataSource = new MatTableDataSource<DatasetValue>();
-  datasetTableLowerIndex = 0;
-  datasetTableUpperIndex = 20;
+  datasetTableLowerIndex = (this.datasetPointsPerPage * -1);
+  datasetTableUpperIndex = 0;
   totalDatasetTablePages = 0;
 
   constructor(private tablePaginator: TablePaginatorService) { }
@@ -42,42 +44,54 @@ export class SpTableComponent implements OnInit {
     }
   }
 
+  // TODO Colocar limitação de disabled para next page em contorno
+
   initializeDatasetTable(): void {
     this.datasetDisplayedColumns = ['Identificador', this.layer.latitudeHeader, this.layer.longitudeHeader, this.layer.dataHeader];
-    this.totalDatasetTablePages = this.tablePaginator.calculateTotalTablePages(this.layer.datasetLength, 20);
-    for (let i = 0; i < 20; i++) {
-      this.datasetTableDataSource.data.push(this.layer.dataset[i]);
-    }
+    this.totalDatasetTablePages = this.tablePaginator.calculateTotalTablePages(this.layer.datasetLength, this.datasetPointsPerPage);
+    this.datasetTableNextPage();
   }
 
   initializeContournTable(): void {
     this.contournDisplayedColumns = ['Identificador', this.contournLatitudeHeader, this.contournLongitudeHeader];
+    this.totalContournTablePages = this.tablePaginator.calculateTotalTablePages(
+      (this.layer as SamplingLayer).contourn.coordinates.length, this.contournPointsPerPage);
     this.hasContourn = true;
     this.contournLatitudeHeader = (this.layer as SamplingLayer).contourn.latitudeHeader;
     this.contournLongitudeHeader = (this.layer as SamplingLayer).contourn.longitudeHeader;
-    for (let i = 0; i < 5; i++) {
-      this.contournTableDataSource.data.push((this.layer as SamplingLayer).contourn.coordinates[i]);
-    }
+    this.contournTableNextPage();
   }
 
   datasetTableNextPage(): void {
-    const nextPageData = this.tablePaginator.nextPage(this.layer.datasetLength, 20, this.datasetTableUpperIndex, this.layer.dataset);
+    const nextPageData = this.tablePaginator.nextPage(this.layer.datasetLength, this.datasetPointsPerPage, this.datasetTableUpperIndex,
+      this.layer.dataset);
     this.datasetTableDataSource.data = nextPageData;
-    this.datasetTableLowerIndex = this.datasetTableLowerIndex + 20;
+    this.datasetTableLowerIndex = this.datasetTableLowerIndex + this.datasetPointsPerPage;
     this.datasetTableUpperIndex = this.datasetTableUpperIndex + nextPageData.length;
-    this.actualDatasetTablePage = this.tablePaginator.calculateActualTablePage(this.layer.datasetLength, 20,
+    this.actualDatasetTablePage = this.tablePaginator.calculateActualTablePage(this.layer.datasetLength, this.datasetPointsPerPage,
+      this.datasetTableUpperIndex, this.totalDatasetTablePages);
+  }
+
+  datasetTablePreviousPage(): void {
+    const newUpperIndex = this.datasetTableUpperIndex - this.datasetTableDataSource.data.length;
+    const previousPageData = this.tablePaginator.previousPage(this.layer.datasetLength, this.datasetPointsPerPage,
+      this.datasetTableUpperIndex, this.layer.dataset);
+    this.datasetTableDataSource.data = previousPageData;
+    this.datasetTableUpperIndex = newUpperIndex;
+    this.datasetTableLowerIndex = this.datasetTableLowerIndex - this.datasetPointsPerPage;
+    this.actualDatasetTablePage = this.tablePaginator.calculateActualTablePage(this.layer.datasetLength, this.datasetPointsPerPage,
       this.datasetTableUpperIndex, this.totalDatasetTablePages);
   }
 
   contournTableNextPage(): void {
     const zmlayer = (this.layer as SamplingLayer);
-    const nextpageData = this.tablePaginator.nextPage(zmlayer.contourn.coordinates.length, 20, this.contournTableUpperIndex,
-      zmlayer.contourn.coordinates);
+    const nextpageData = this.tablePaginator.nextPage(zmlayer.contourn.coordinates.length, this.contournPointsPerPage,
+      this.contournTableUpperIndex, zmlayer.contourn.coordinates);
     this.contournTableDataSource.data = nextpageData;
-    this.contournTableLowerIndex = this.datasetTableLowerIndex + 20;
-    this.datasetTableUpperIndex = this.datasetTableUpperIndex + nextpageData.length;
-    this.actualDatasetTablePage = this.tablePaginator.calculateActualTablePage(zmlayer.contourn.coordinates.length, 20,
-      this.contournTableUpperIndex, this.totalContournTablePages);
+    this.contournTableLowerIndex = this.datasetTableLowerIndex + this.contournPointsPerPage;
+    this.contournTableUpperIndex = this.datasetTableUpperIndex + nextpageData.length;
+    this.actualContournTablePage = this.tablePaginator.calculateActualTablePage(zmlayer.contourn.coordinates.length,
+      this.contournPointsPerPage, this.contournTableUpperIndex, this.totalContournTablePages);
   }
 
   /*tablePreviousPage(): void {
