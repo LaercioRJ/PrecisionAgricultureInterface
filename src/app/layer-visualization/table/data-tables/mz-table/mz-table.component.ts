cbeeebde531fc50ce4a4f942c2720e8d136cdffc
unknown <laerciorjunior@gmail.com>
2021-07-03 13:59:18 -0300

    layer-table-editing 1

--- src/app/layer-visualization/table/data-tables/mz-table/mz-table.component.ts
+++ src/app/layer-visualization/table/data-tables/mz-table/mz-table.component.ts
@@ -1,9 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { MatTableDataSource } from '@angular/material/table';
 
 import { DatasetValue } from '../../../../classes/datasetValue';
 import { Layer } from '../../../../classes/layer';
 
+import { LayerStorageService } from '../../../../services/layer-storage.service';
+import { MessageDeliveryService } from '../../../../services/message-delivery.service';
+import { NumberInputValidationService } from '../../../../services/validation/number-input-validation.service';
 import { TablePaginatorService } from '../../../../services/table-paginator.service';
 
 @Component({
@@ -14,15 +18,22 @@ import { TablePaginatorService } from '../../../../services/table-paginator.serv
 export class MzTableComponent implements OnInit {
   @Input() layer!: Layer;
 
+  alteredpointsId: number[] = [];
   actualTablePage = 1;
   datasetTableDataSource = new MatTableDataSource<DatasetValue>();
   datasetDisplayedColumns!: string[];
+  isEditing = false;
   linesPerPage = 20;
   tableLowerIndex = (this.linesPerPage * -1);
   tableUpperIndex = 0;
   totalTablePages = 0;
+  wasEdited = false;
 
-  constructor(private tablePaginator: TablePaginatorService) { }
+  constructor(private activatedRoute: ActivatedRoute,
+              private layerStorage: LayerStorageService,
+              private messageDelivery: MessageDeliveryService,
+              private numberValidation: NumberInputValidationService,
+              private tablePaginator: TablePaginatorService) { }
 
   ngOnInit(): void {
     this.totalTablePages = this.tablePaginator.calculateTotalTablePages(this.layer.datasetLength, this.linesPerPage);
@@ -70,6 +81,46 @@ export class MzTableComponent implements OnInit {
     this.tableLowerIndex = this.tableLowerIndex - (this.linesPerPage * 10);
     this.actualTablePage = this.tablePaginator.calculateActualTablePage(this.layer.datasetLength, this.linesPerPage,
       this.tableUpperIndex, this.totalTablePages);
+  }
+
+  editTableData(event: any, tablePointIndex: number): void {
+    this.wasEdited = true;
+    let dirtForm = false;
+    const newInput = Number(event.target.value);
+
+    if (!this.numberValidation.isInteger(newInput)) {
+      this.messageDelivery.showMessage('Por favor, adicione apenas números inteiros !', 2200);
+      dirtForm = true;
+    }
+    if (!this.numberValidation.isWithinBounds(newInput, 1, 10)) {
+      this.messageDelivery.showMessage('Por favor, adicione apenas números maiores do que 0 ou menores que 11 !', 2500);
+      dirtForm = true;
+    }
+
+    if (dirtForm) {
+      event.target.value = this.layer.dataset[tablePointIndex + this.tableLowerIndex].data;
+    } else {
+      this.layer.dataset[tablePointIndex + this.tableLowerIndex] = this.datasetTableDataSource.data[tablePointIndex];
+      this.alteredpointsId.push(tablePointIndex + this.tableLowerIndex);
+    }
+  }
+
+  deleteAlterations(): void {
+    const layerIndex = Number(this.activatedRoute.snapshot.paramMap.get('layerIndex'));
+
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < this.alteredpointsId.length; i++) {
+      const pointOldDataset = this.layerStorage.getLayerPoint(layerIndex, this.alteredpointsId[i]);
+      this.layer.dataset[this.alteredpointsId[i]].data = pointOldDataset.data;
+    }
+
+    for (let i = 0; i < this.linesPerPage; i++ ) {
+      this.datasetTableDataSource.data[i].data = this.layer.dataset[i].data;
+    }
+
+    this.wasEdited = false;
+    this.alteredpointsId = [];
+    this.messageDelivery.showMessage('Todas as alterações foram desfeitas com sucesso !', 2200);
   }
 
 }
